3190ecfe67a24e284f5f4d59f892aa563c0fafef
Walmyr <wlsf82@gmail.com>
2023-05-09 19:50:20 +0200

    Solve extra exercise of lesson 2

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -1,27 +1,14 @@
 import { faker } from '@faker-js/faker/locale/en'
 
 describe('Sign up', () => {
+  const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
+  const password = Cypress.env('USER_PASSWORD')
+
   it('successfully signs up using confirmation code sent via email', () => {
-    const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
-    const password = Cypress.env('USER_PASSWORD')
+    cy.fillSignupFormAndSubmit(emailAddress, password)
 
-    cy.intercept('GET', '**/notes').as('getNotes')
-    cy.visit('/signup')
-    cy.get('#email').type(emailAddress)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
-
-    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-      sentTo: emailAddress
-    }).then(message => {
-      const confirmationCode = message.html.body.match(/\d{6}/)[0]
-      cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-
-      cy.wait('@getNotes')
-      cy.contains('h1', 'Your Notes').should('be.visible')
-    })
+    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+    cy.wait('@getNotes')
+  })
+})
+
