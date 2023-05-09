b6b4329172c3fc88ae625424cd05f5a661dbffc9
Walmyr <wlsf82@gmail.com>
2023-05-09 20:04:40 +0200

    Solve extra exercise 2 of lesson 3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,13 +4,9 @@ describe('Login', () => {
   it('successfully logs in', () => {
     cy.intercept('GET', '**/notes').as('getNotes')
 
-    cy.visit('/login')
-    cy.get('#email').type(Cypress.env('USER_EMAIL'))
-    cy.get('#password').type(Cypress.env('USER_PASSWORD'), { log: false })
-    cy.contains('button', 'Login').click()
+    cy.login()
     cy.wait('@getNotes')
 
-    cy.contains('h1', 'Your Notes').should('be.visible')
     cy.contains('a', 'Create a new note').should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,3 +15,14 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   })
 })
 
+Cypress.Commands.add('login', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  cy.visit('/login')
+  cy.get('#email').type(username)
+  cy.get('#password').type(password, { log: false })
+  cy.contains('button', 'Login').click()
+  cy.contains('h1', 'Your Notes', { timeout: 10000 }).should('be.visible')
+})
+
