92c108bae08647616a8fea9aa5f29f8f407c8d7f
Walmyr <wlsf82@gmail.com>
2023-05-09 19:47:29 +0200

    Solve the 1st exercise of lesson 2

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://notes-serverless-app.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
