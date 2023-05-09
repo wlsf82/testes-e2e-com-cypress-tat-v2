6a34515899891383f970119f792ad59b06fa5df5
Walmyr <wlsf82@gmail.com>
2023-05-09 20:54:55 +0200

    Solve extra exercise 1 of lesson 7

--- cypress.config.js
+++ cypress.config.js
@@ -7,8 +7,6 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
+  projectId: 'ywd6nd',
 })
