4110f10502a77c96687c66b0d34ebd40e23293ea
Walmyr <wlsf82@gmail.com>
2023-05-09 20:11:07 +0200

    Solve extra exercise 3 of lesson 3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,10 +2,7 @@
 
 describe('Login', () => {
   it('successfully logs in', () => {
-    cy.intercept('GET', '**/notes').as('getNotes')
-
-    cy.login()
-    cy.wait('@getNotes')
+    cy.guiLogin()
 
     cy.contains('a', 'Create a new note').should('be.visible')
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -15,14 +15,24 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   })
 })
 
-Cypress.Commands.add('login', (
+Cypress.Commands.add('guiLogin', (
   username = Cypress.env('USER_EMAIL'),
   password = Cypress.env('USER_PASSWORD')
 ) => {
+  cy.intercept('GET', '**/notes').as('getNotes')
   cy.visit('/login')
   cy.get('#email').type(username)
   cy.get('#password').type(password, { log: false })
   cy.contains('button', 'Login').click()
-  cy.contains('h1', 'Your Notes', { timeout: 10000 }).should('be.visible')
+  cy.wait('@getNotes')
+  cy.contains('h1', 'Your Notes').should('be.visible')
+})
+
+Cypress.Commands.add('sessionLogin', (
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD')
+) => {
+  const login = () => cy.guiLogin(username, password)
+  cy.session(username, login)
 })
 
